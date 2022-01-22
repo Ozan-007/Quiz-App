d53b615d56d28464a10c3f923400a9db73199281
Ozan <ozan28061@gmail.com>
2022-01-22 22:04:42 +0100

    Question model added, verbose name plural added

--- quiz_app/admin.py
+++ quiz_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from quiz_app.models import Category, Quiz
+
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Quiz)
--- quiz_app/models.py
+++ quiz_app/models.py
@@ -8,10 +8,25 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = "Categories"
+
 
 class Quiz(models.Model):
     title = models.CharField(max_length=50)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name_plural = "Quizzes"
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    title = models.CharField(max_length=300, verbose_name="question")
+
 
     def __str__(self):
         return self.title
