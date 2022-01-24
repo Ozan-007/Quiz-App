a3692a87422514c72fc20940988240c6402b9e1c
Ozan <ozan28061@gmail.com>
2022-01-24 21:23:31 +0100

    admin.py updated changes happened in admin panel

--- quiz_app/admin.py
+++ quiz_app/admin.py
@@ -2,8 +2,23 @@ from django.contrib import admin
 
 from quiz_app.models import Answer, Category, Quiz, Question
 
+import nested_admin
+
 # Register your models here.
+
+class AnswerInline(nested_admin.NestedStackedInline):
+    model = Answer
+
+class QuestionInline(nested_admin.NestedStackedInline):
+    model = Question
+    inlines = [AnswerInline]
+
+class QuizAdmin(nested_admin.NestedModelAdmin):
+    model = Quiz
+    inlines = [QuestionInline]
+
+
 admin.site.register(Category)
-admin.site.register(Quiz)
+admin.site.register(Quiz, QuizAdmin)
 admin.site.register(Question)
 admin.site.register(Answer)
