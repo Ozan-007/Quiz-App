7b7fbbdf8d4850b8d5ce51e23289491a72a7d3da
Ozan <ozan28061@gmail.com>
2022-02-04 18:18:06 +0100

    authentication added

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     #third party
     'rest_framework',
     'nested_admin',
+    'rest_framework.authtoken',
     # my-apps
     'quiz_app',
     'users',
@@ -134,5 +135,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 1
+    'PAGE_SIZE': 1,
+     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+
 }
